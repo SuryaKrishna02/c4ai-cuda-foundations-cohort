c6ac30896da1518322cd109acd4ebe46b802f6ab
SuryaKrishna02 <suryakguthikonda@gmail.com>
2024-05-13 23:09:00 +0530

    Changes in `add_vectors.cu` file

--- cuda_advanced/add_vectors.cu
+++ cuda_advanced/add_vectors.cu
@@ -13,10 +13,9 @@ using namespace std;
 
 __global__ void add(int *a, int *b, int *c)
 {
-    for (int i = 0; i < N; i++)
-    {
-        c[i] = a[i] + b[i];
-    }
+    int tid = blockIdx.x; // handle the data at this index
+    if (tid < N)
+        c[tid] = a[tid] + b[tid];
 }
 
 int main(void)
