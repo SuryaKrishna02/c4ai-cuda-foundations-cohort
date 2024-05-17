d7dc8fc7716f1369698e8e86c52e6d850b5db5d5
SuryaKrishna02 <suryakguthikonda@gmail.com>
2024-05-17 23:08:39 +0530

    Updated timer scripts

--- cuda_advanced/best_practices/HighResolutionTimer.cpp
+++ cuda_advanced/best_practices/HighResolutionTimer.cpp
@@ -2,6 +2,27 @@
 #include <windows.h>
  
 #include "HighResolutionTimer.h"
+
+HighResolutionTimerImpl::HighResolutionTimerImpl()
+: elapsedTime(0)
+{
+    QueryPerformanceFrequency(&frequency);
+    QueryPerformanceCounter(&t0);
+}
+ 
+void HighResolutionTimerImpl::Tick()
+{
+    QueryPerformanceCounter(&t1);
+    // Compute the value in microseconds (1 second = 1,000,000 microseconds)
+    elapsedTime = ( t1.QuadPart - t0.QuadPart ) * ( 1000000.0 / frequency.QuadPart );
+ 
+    t0 = t1;
+}
+ 
+double HighResolutionTimerImpl::GetElapsedTimeInMicroSeconds()
+{
+    return elapsedTime;
+}
  
 class HighResolutionTimerImpl
 {
